239064785f12fce712c41ced080028d9e8588c6f
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-18 19:37:52 +0400

    task-3

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,15 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+    let input = result.input
+    let numbers = input.split(',')
+    let a = parseInt(numbers[0])
+    let b = parseInt(numbers[1])
+    let c = parseInt(numbers[2])
+    if (a + b > c && a + c > b && b + c > a) {
+        console.log("YES");
+    } else {
+        console.log("NO");
+    }
+    });
