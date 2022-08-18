1b56df24b5f7e7989ca061a3c16206aa75462bc3
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-18 20:08:26 +0400

    task-4

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,24 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+    let input = result.input;
+    let n = input.split("");
+    let a = parseInt(n[0]);
+    let b = parseInt(n[1]);
+    let c = parseInt(n[2]);
+    let d = parseInt(n[3]);
+
+    let count = 1
+    if (a === b) {
+        count++;
+    }
+    if (a === d) {
+        count++;
+    }
+    if (count === 3) {
+        console.log("YES");
+    } else {
+        console.log("NO")
+    }
 });
