fe78a82e4dd252d9a9084a0a08ece981a77b0bb0
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-18 19:03:58 +0400

    task-1

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+    let x = parseInt(result.input);
+    let num;
+    if (-10000 <= x && x <= 10000) {
+        if (x >= 10) {
+            num = x ** 3 + x * 5;
+        } else {
+            num = x ** 2 - 2 * x + 4;
+        }
+    }
+    console.log(num);
 });
