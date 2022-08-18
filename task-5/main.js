e9eb766238793a70932980dee3ef6fd3250c8470
Selim Mammadov <mamedovfhn@gmail.com>
2022-08-18 20:27:42 +0400

    task-5

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,13 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let input = result.input.split("");
+    for (let i = 0; i < input.length; i++) {
+        if (parseInt(input[i]) === 3 && parseInt(input[i + 1]) === 7) {
+            console.log("yes");
+        } else {
+            console.log("no");
+        }
+    }
 });
